cd9a855b1991ef25e785700fafb57ddc13958578
Mit Amin <aminm2@vcu.edu>
2018-03-16 01:27:26 -0400

    first backend commit

---
+++ src/main/java/com/kappagamma/website/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.kappagamma.website.web.rest;
---
+++ src/main/java/com/kappagamma/website/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.kappagamma.website.web.rest.vm;
